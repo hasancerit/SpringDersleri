9f6801e309f963d9b8f7cc085c8ada6fe9014b2e
hasancerit <hasan__cerit@hotmail.com>
2019-11-16 05:05:57 +0300

    thymFragments

---
+++ ThymleafExample/src/main/java/com/example/thymleaf/ThymleafExample/ThymleafExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymleaf.ThymleafExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymleafExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymleafExampleApplication.class, args);
+	}
+
+}

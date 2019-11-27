779877fb776ea663c44fda8ab918a733271ff6e7
hasancerit <hasan__cerit@hotmail.com>
2019-11-27 23:03:19 +0300

    fragments

--- ThymleafExample/src/main/java/com/example/thymleaf/ThymleafExample/ThymleafExampleApplication.java
+++ ThymleafExample/src/main/java/com/example/thymleaf/ThymleafExample/ThymleafExampleApplication.java
@@ -2,6 +2,9 @@ package com.example.thymleaf.ThymleafExample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.UrlTemplateResolver;
 
 @SpringBootApplication
 public class ThymleafExampleApplication {
--- ThymleafExample/src/main/java/com/example/thymleaf/ThymleafExample/controller/IndexController.java
+++ ThymleafExample/src/main/java/com/example/thymleaf/ThymleafExample/controller/IndexController.java
@@ -1,13 +1,37 @@
 package com.example.thymleaf.ThymleafExample.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 		
-	@GetMapping("/")
-	public String getIndex() {
+	@GetMapping("/{id}")
+	public String getIndex(@PathVariable String id,Model model) {
+		if(id.equalsIgnoreCase("1")) {
+			model.addAttribute("isim","Hasan");
+			model.addAttribute("isAdmin",true);
+		}else {
+			model.addAttribute("isim","Misafir");
+			model.addAttribute("isAdmin",false);
+		}
 		return "index";
+		
+	}
+	
+	@ResponseBody
+	@GetMapping("/getdatarp")
+	public String getDataReqParam(@RequestParam String id) {
+		return id;
+	}
+	
+	@ResponseBody
+	@GetMapping("/getdatama")
+	public String getDataModelAttribute(@RequestParam String id) {
+		return id;
 	}
 }

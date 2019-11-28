3b42fea64f0ac352cf299786c135c3fcc8f1acec
hasancerit <hasan__cerit@hotmail.com>
2019-11-28 20:16:18 +0300

    recipe oo

---
+++ RecipeApp/src/main/java/guru/springframework/RecipeApp/Notes.java
@@ -0,0 +1,50 @@
+package guru.springframework.RecipeApp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+import guru.springframework.RecipeApp.model.Recipe;
+
+@Entity
+public class Notes {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Lob
+	private String recipeNotes;
+	
+	@OneToOne()
+	private Recipe recipe;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getRecipeNotes() {
+		return recipeNotes;
+	}
+
+	public void setRecipeNotes(String recipeNotes) {
+		this.recipeNotes = recipeNotes;
+	}
+
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+	
+	
+}
